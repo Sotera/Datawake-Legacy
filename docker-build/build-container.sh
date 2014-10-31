cdeabd1d094b6e419762e3f01b057fe64d0e592d
Eric Kimbrel <eric.kimbrel@soteradefense.com>
2014-10-31 09:51:06 -0700

    dockerfile / build fixes

---
+++ docker-build/build-container.sh
@@ -0,0 +1,108 @@
+#! /bin/bash
+
+# install pip and python dev tools
+echo "install pip and python dev tools"
+apt-get update
+apt-get upgrade
+apt-get install -y python-software-properties
+apt-get -y install python-pip
+apt-get -y install python-dev
+
+
+# install dependencies required to build tangelo
+
+# needed until sessions are supported in offical tangelo release
+echo "install dependencies required to build tangelo"
+apt-add-repository -y ppa:chris-lea/node.js
+apt-get install -y nodejs
+apt-get install -y npm
+ln -s /usr/bin/nodejs /usr/bin/node
+apt-get -y install git
+apt-get -y install make
+apt-get -y install cmake
+
+
+
+# install tangelo from sessions branch
+
+echo "build/install tangelo"
+git clone https://github.com/Kitware/tangelo.git
+cd tangelo
+git checkout sessions
+cd ..
+mkdir tangelo-build
+cd tangelo-build
+cmake -DNPM_EXECUTABLE=/usr/bin/npm  -DBUILD_DOCUMENTATION=OFF -DBUILD_TESTING=OFF ../tangelo
+make
+pip install sdist/tangelo-0.7.tar.gz
+cd ..
+
+
+
+# misc python libs
+
+echo "install misc python libs"
+pip install happybase
+pip install httplib2
+
+
+# install wget
+
+echo "install wget"
+apt-get install -y wget
+
+
+# install mysql client
+
+echo "install mysql client"
+wget http://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-1.2.3.tar.gz
+tar xf mysql-connector-python-1.2.3.tar.gz
+cd mysql-connector-python-1.2.3/
+python setup.py install &> /dev/null
+cd ..
+
+
+
+# install igraph
+
+echo "install igraph"
+add-apt-repository -y ppa:igraph/ppa
+apt-get update
+apt-get install -y python-igraph
+
+
+
+# install kafka python client
+
+echo "install kafka python client"
+wget https://github.com/mumrah/kafka-python/releases/download/v0.9.2/kafka-python-0.9.2.tar.gz
+tar -xzvf kafka-python-0.9.2.tar.gz
+cd kafka-python-0.9.2/
+python setup.py install
+cd ..
+
+
+# install impyla
+
+echo "install impyla"
+git clone https://github.com/cloudera/impyla.git &> /dev/null
+cd impyla
+python setup.py install
+cd ..
+
+
+
+# install pyodc
+
+echo "install odbc"
+apt-get -y install aptitude
+aptitude -y install g++
+apt-get -y install unixodbc-dev
+pip install --allow-external pyodbc --allow-unverified pyodbc pyodbc
+
+
+
+echo "software install complete"
+
+
+
