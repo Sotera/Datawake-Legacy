06cdc1108f590fe008e334d080cd4d4ce909a095
Eric Kimbrel <eric.kimbrel@soteradefense.com>
2014-11-13 10:08:26 -0800

    docker build reworked
    Separated web server and streaming into their own container.  Put all
    dependencies into the Dockerfiles.

---
+++ memex-datawake-stream/dev_container_start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# This file executes inside of a docker container to create topics and execute a topology for local dev environments
+# not intended for production deployments.
+
+
+$KAFKA_HOME/bin/kafka-run-class.sh kafka.admin.DeleteTopicCommand --zookeeper $ZK_PORT_2888_TCP_ADDR --topic memex-datawake-visited
+$KAFKA_HOME/bin/kafka-run-class.sh kafka.admin.DeleteTopicCommand --zookeeper $ZK_PORT_2888_TCP_ADDR --topic crawler-out
+$KAFKA_HOME/bin/kafka-run-class.sh kafka.admin.DeleteTopicCommand --zookeeper $ZK_PORT_2888_TCP_ADDR --topic crawler-in
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper $ZK_PORT_2888_TCP_ADDR --replication-factor 1 --partitions 1 --topic memex-datawake-visited
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper $ZK_PORT_2888_TCP_ADDR --replication-factor 1 --partitions 1 --topic crawler-in
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper $ZK_PORT_2888_TCP_ADDR --replication-factor 1 --partitions 1 --topic crawler-out
+
+sparse run -n local -t 9999999 -o "'topology.deployment=\"local-docker\"'"
