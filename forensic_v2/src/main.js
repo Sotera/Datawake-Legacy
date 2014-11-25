f34bac576ce7036ea65c18170337e05b83d340b6
chris-dickson <chris.r.dickson@gmail.com>
2014-11-25 16:50:30 -0500

    Clean up main.js a bit.

--- forensic_v2/src/main.js
+++ forensic_v2/src/main.js
@@ -5,31 +5,40 @@
 require(['config','views/navbarView', 'views/graphView', 'util/util', 'util/guid'], function(config,NavbarView,GraphView,util,guid) {
 	require([],
 		function() {
+			var getTrails = function() {
+				var d = new $.Deferred();
+				$.get('/datawake/forensic/graphservice/trails')
+					.then(function(response) {
+						var trails = [];
+						try {
+							var JSONResponse = JSON.parse(response);
+							JSONResponse.forEach(function (trail) {
+								if (util.hasProperties(trail)) {
+									trail.id = guid.generate();
+									trails.push(trail);
+								}
+							});
+						} catch(err){
+							console.error('Server returned no trails!');
+						}
+						d.resolve(trails);
+					})
+					.fail(function(err) {
+						d.reject(err);
+					});
+				return d.promise();
+			};
+
+			/*----------------------------------------------------------------------------------------------------------
+			 *		Program Entry
+			 *--------------------------------------------------------------------------------------------------------*/
 			var _graphView = null;
 			var _navbarView = null;
-
-
-			$.get('/datawake/forensic/graphservice/trails')
-				.then(function(response) {
-					var trails = [];
-					try {
-						var JSONResponse = JSON.parse(response);
-						JSONResponse.forEach(function (trail) {
-							if (util.hasProperties(trail)) {
-								trail.id = guid.generate();
-								trails.push(trail);
-							}
-						});
-					} catch(err){
-						console.error('Server returned no trails!');
-					}
-					_navbarView = new NavbarView($('#navbarContainer'),{
-						trails:trails
-					});
-				})
-				.fail(function(err) {
-					alert('failed!');
+			getTrails().then(function(trails) {
+				_navbarView = new NavbarView($('#navbarContainer'),{
+					trails:trails
 				});
-			_graphView = new GraphView($('#graphContainer'),{});
+				_graphView = new GraphView($('#graphContainer'),{});
+			});
 		});
 });
