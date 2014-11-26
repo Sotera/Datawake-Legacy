cd40f52d66cc0d197f6a9b2b2879f264504100d7
chris-dickson <chris.r.dickson@gmail.com>
2014-11-25 19:06:48 -0500

    Add optional 'self' parameter when subscribing to messages so that 'this' is correct in the callback

--- forensic_v2/src/util/events.js
+++ forensic_v2/src/util/events.js
@@ -1,14 +1,17 @@
 define([], function() {
 	var topics = {};
 	return {
-		subscribe: function(topic, listener) {
+		subscribe: function(topic, listener, self) {
 			// Create the topic's object if not yet created
 			if(!topics[topic]) {
 				topics[topic] = { queue: [] };
 			}
 
 			// Add the listener to queue
-			var index = topics[topic].queue.push(listener) -1;
+			var index = topics[topic].queue.push({
+					func : listener,
+					self : self
+				}) -1;
 
 			// Provide handle back for removal of topic
 			return {
@@ -19,12 +22,18 @@ define([], function() {
 		},
 		publish: function(topic, info) {
 			// If the topic doesn't exist, or there's no listeners in queue, just leave
-			if(!topics[topic] || !topics[topic].queue.length) return;
+			if(!topics[topic] || !topics[topic].queue.length) {
+				return;
+			}
 
 			// Cycle through topics queue, fire!
 			var items = topics[topic].queue;
 			items.forEach(function(item) {
-				item(info || {});
+				if (item.self) {
+					item.func.call(item.self, info || {});
+				} else {
+					item(info || {});
+				}
 			});
 		},
 		topics: {
