91a2c5e03236dcf40397bff87ad9f477e03731e9
cdickson <cdickson@oculusinfo.com>
2015-01-05 15:17:22 -0500

    Add password protection to demo server

--- ForensicDemoServer/server.js
+++ ForensicDemoServer/server.js
@@ -1,4 +1,15 @@
 var express = require('express');
+var auth = require('http-auth');
+var basic = auth.basic({
+        realm: 'Datawake Forensic',
+        file: './.htpassword'
+    }
+);
+
+// Application setup.
 var app = express();
-app.use('/', express.static('../server/forensic_v2'));
+app.use(auth.connect(basic));
+
+// Setup route.
+app.use('/',express.static('../server/forensic_v2'));
 app.listen(9006);
