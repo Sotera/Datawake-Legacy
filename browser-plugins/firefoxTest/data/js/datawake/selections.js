79231142d3a81e75380d068c56e567385c2444dd
BTWhiteman <bwhiteman@gmail.com>
2015-03-11 09:19:16 -0400

    removing unused addon

--- browser-plugins/firefoxTest/data/js/datawake/selections.js
+++
@@ -1,8 +0,0 @@
-self.on("click", function (node, contextItemDataProperty) {
-    var messageWrapper = {};
-    if (contextItemDataProperty == "selection" || contextItemDataProperty == "feedback" || contextItemDataProperty == "add-trail-entity") {
-        messageWrapper.text = window.getSelection().toString();
-    }
-    messageWrapper.intent = contextItemDataProperty;
-    self.postMessage(messageWrapper);
-});
