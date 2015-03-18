4f41e0425f18bda8b2325c25bdc9b696bb87e264
BTWhiteman <bwhiteman@gmail.com>
2015-03-18 14:35:02 -0400

    refreshing entities/trails and removing spaces from urls

--- browser-plugins/FirefoxSidebar/data/js/datawake/panel.js
+++ browser-plugins/FirefoxSidebar/data/js/datawake/panel.js
@@ -14,19 +14,11 @@ panelApp.controller("PanelCtrl", function($scope, $document) {
   $scope.domainSpinner = true;
   $scope.trailSpinner = true;
   $scope.extracted_tools = [];
-  // $scope.datawake = addon.options.datawakeInfo;
-  // $scope.current_url = addon.options.current_url;
-  // $scope.domainFeaturesEnabled = addon.options.useDomainFeatures;
-  // $scope.rankingEnabled = addon.options.useRanking;
-  // $scope.versionNumber = addon.options.versionNumber;
-  // $scope.user = addon.options.userInfo;
   $scope.invalid = {};
-  // $scope.pageVisits = addon.options.pageVisits;
   $scope.headerPartial = "partials/header-partial.html";
   $scope.createTrailPartial = "partials/trail-modal-partial.html";
   $scope.createDomainPartial = "partials/domain-modal-partial.html";
   $scope.createTeamPartial = "partials/team-modal-partial.html"
-    // $scope.domains = addon.options.domains;
   $scope.teamMembers = null;
   if (!$scope.domains) $scope.domains = [];
   $scope.trails = []
@@ -36,6 +28,10 @@ panelApp.controller("PanelCtrl", function($scope, $document) {
     console.log("Datawake Prefs")
     console.log(prefs.datawakeInfo)
     $scope.datawake = prefs.datawakeInfo;
+
+    $scope.datawake.domain = 'memex'
+    $scope.datawake.trail = 'trail'
+
     $scope.current_url = prefs.current_url;
     $scope.lookaheadEnabled = prefs.useLookahead;
     $scope.domainFeaturesEnabled = prefs.useDomainFeatures;
@@ -45,220 +41,7 @@ panelApp.controller("PanelCtrl", function($scope, $document) {
     $scope.starUrl = prefs.starUrl
   });
 
-
-  //  console.log("opening panel for tab: "+addon.options.tabId)
-  console.log($scope.datawake)
-
-  // TEAMS
-  $scope.teams = [];
-  if ($scope.datawake && $scope.datawake.team) $scope.teams.push($scope.datawake.team)
-  $scope.selectedTeam = ($scope.datawake && $scope.datawake.team) ? $scope.datawake.team : null;
-
-  // when teams are returned  from the server set the selected team as the matching element from the teams array
-  addon.port.on("teams", function(teams) {
-    $scope.teams = teams;
-    $scope.selectedTeam = null;
-    if ($scope.datawake.team) {
-      for (i in $scope.teams) {
-        if ($scope.teams[i].id == $scope.datawake.team.id) {
-          $scope.selectedTeam = $scope.teams[i]
-        }
-      }
-    }
-    $scope.teamSpinner = false;
-    $scope.$apply();
-    console.log("GOT TEAMS")
-    console.log(teams)
-  });
-
-  $scope.teamChanged = function(team) {
-    $scope.datawake.team = team;
-    $scope.datawake.domain = null;
-    $scope.datawake.trail = null;
-    $scope.isDatawakeOn = false;
-    $scope.domains = []
-    $scope.trails = []
-    $scope.domainSpinner = true;
-    addon.port.emit("changeTeam", {
-      tabId: addon.options.tabId,
-      team: team
-    });
-    console.log("teamChanged")
-    console.log($scope.datawake)
-  };
-
-  $scope.teamChangedOnTeamModal = function(team) {
-    $scope.teamMembers = null;
-    addon.port.emit("getTeamMembers", team)
-  }
-
-  addon.port.on("gotTeamMembers", function(teamMembers) {
-    $scope.teamMembers = teamMembers
-    console.log("got team members")
-    console.log(teamMembers)
-    $scope.$apply();
-  })
-
-
-  $scope.createTeam = function(name, description) {
-    console.log("create team: (" + name + "," + description + ")")
-    var data = {
-      name: name,
-      description: description
-    }
-    $scope.newTeamDisabled = true;
-    addon.port.emit("createTeam", data)
-  }
-
-  addon.port.on("teamCreated", function(team) {
-    console.log("teamCreated")
-    console.log(team)
-    $scope.newTeamDisabled = false;
-    $scope.datawake.team = team;
-    $scope.teams.push(team)
-    $scope.$apply()
-  })
-
-  $scope.addTeamMemeber = function(team, email) {
-    console.log("adding team member")
-    console.log(team)
-    console.log(email)
-    var data = {
-      team_id: team.id,
-      email: email
-    }
-    addon.port.emit("addTeamMember", data)
-
-  }
-
-  $scope.removeTeamMember = function(team, email) {
-    console.log("removing team member")
-    console.log(team)
-    console.log(email)
-    var data = {
-      team_id: team.id,
-      email: email
-    }
-    addon.port.emit("removeTeamMember", data)
-  }
-
-
-  // DOMAINS
-  // if (! $scope.datawake.team) $scope.domainSpinner = false;
-  // $scope.domains =[];
-  // if ($scope.datawake && $scope.datawake.domain ) $scope.domains.push($scope.datawake.domain)
-  // $scope.selectedDomain = ($scope.datawake && $scope.datawake.domain ) ? $scope.datawake.domian : null;
-
-  // when domains come in from the server set the selected domain to the matching element
-  addon.port.on("domains", function(domains) {
-    $scope.selectedDomain = null;
-    $scope.domains = domains;
-    if ($scope.datawake.domain && $scope.domains) {
-      for (i in $scope.domains) {
-        if ($scope.domains[i].id == $scope.datawake.domain.id) {
-          $scope.selectedDomain = $scope.domains[i]
-        }
-      }
-    }
-    $scope.domainSpinner = false;
-    $scope.$apply()
-    console.log("GOT DOMAINS")
-    console.log(domains)
-  });
-
-  $scope.domainChanged = function(domain) {
-    $scope.datawake.domain = domain;
-    $scope.datawake.trail = null;
-    $scope.isDatawakeOn = false;
-    $scope.trails = [];
-    $scope.trailSpinner = true;
-    addon.port.emit("changeDomain", {
-      tabId: addon.options.tabId,
-      domain: $scope.datawake.domain
-    });
-    console.log("domainChanged")
-    console.log($scope.datawake)
-
-  };
-
-
-  // TRAILS
-  // if ( !$scope.datawake.team || ! $scope.datawake.domain ) $scope.trailSpinner = false;
-  // $scope.trails = [];
-  // if ($scope.datawake && $scope.datawake.trail) $scope.trails.push($scope.datawake.trail);
-  // $scope.selectedTrail = ($scope.datawake && $scope.datawake.trail) ? $scope.datawake.trail : null;
-
-  addon.port.on("trails", function(trails) {
-    $scope.trails = trails;
-    $scope.selectedTrail = null;
-    if ($scope.datawake.trail && $scope.trails) {
-      for (i in $scope.trails) {
-        if ($scope.trails[i].id == $scope.datawake.trail.id) {
-          $scope.selectedTrail = $scope.trails[i]
-        }
-      }
-    }
-    $scope.trailSpinner = false;
-    $scope.$apply();
-    console.log("GOT TRAILS")
-    console.log(trails)
-  });
-
-  $scope.trailChanged = function(trail) {
-    $scope.datawake.trail = trail;
-    $scope.datawake.isDatawakeOn = false;
-    addon.port.emit("infochanged", {
-      tabId: addon.options.tabId,
-      info: $scope.datawake
-    });
-    console.log("trailChanged")
-    console.log($scope.datawake)
-  };
-
-
-  $scope.newTrail = function(team, domain, newTrailName, newTrailDesc) {
-    var data = {}
-    data.team_id = team.id;
-    data.domain_id = domain.id;
-    data.name = newTrailName
-    data.description = (newTrailDesc) ? newTrailDesc : "";
-    $scope.trailChanged(null);
-    $scope.newTrailName = null;
-    addon.port.emit("createTrail", data);
-  }
-
-
-  addon.port.on("trailCreated", function(trail) {
-    $scope.trails.push(trail)
-    $scope.selectedTrail = trail
-    $scope.datawake.trail = trail;
-    addon.port.emit("infochanged", {
-      tabId: addon.options.tabId,
-      info: $scope.datawake
-    });
-    $scope.apply()
-  });
-
-
-
-  // Recording
-  $scope.recordingChange = function(recording) {
-    $scope.datawake.isDatawakeOn = recording;
-    addon.port.emit("infochanged", {
-      tabId: addon.options.tabId,
-      info: $scope.datawake
-    });
-    console.log("recordingChange")
-    console.log($scope.datawake)
-  }
-
-  addon.port.on("manualFeatures", function(features) {
-    console.log("Got manual features")
-    console.log(features)
-    $scope.$apply(function() {
-      $scope.manualFeatures = features;
-    });
-  });
+  console.log($scope.datawake);
 
   addon.port.on("trailEntities", function(entities_obj) {
     console.log("Got trail entities")
@@ -352,14 +135,19 @@ panelApp.controller("PanelCtrl", function($scope, $document) {
   }
 
   $scope.getHostName = function(url) {
+    //For some reason, sometimes errant spaces were apearing in the urls.
+    url = url.replace(/\s+/g, '');
     return new URL(url).hostname
   };
 
   $scope.showEntities = function(link) {
     if (!link.show) {
       var data = {};
-      data.domain = $scope.datawake.domain.name;
-      data.trail = $scope.datawake.trail.name;
+      // data.domain = $scope.datawake.domain.name;
+      // data.trail = $scope.datawake.trail.name;
+      data.domain = 'memex';
+      data.trail = 'trail';
+
       data.url = link.url;
       addon.port.emit("getUrlEntities", data);
 
@@ -382,6 +170,14 @@ panelApp.controller("PanelCtrl", function($scope, $document) {
     console.log($scope.datawake)
   })
 
+  $scope.refreshWebPages = function () {
+      addon.port.emit("refreshWebPages", {domain: "memex", trail: "trail"});
+  };
+
+  $scope.refreshEntities = function () {
+      addon.port.emit("refreshEntities", {domain: "memex", trail: "trail"});
+  };
+
   function createIterableEntityListForSorting(entities) {
     var arr = [];
     $.map(entities, function(item, key) {
@@ -394,26 +190,26 @@ panelApp.controller("PanelCtrl", function($scope, $document) {
   }
 
 
-  function createStarRating(starUrl) {
-    var starRating = $("#star_rating");
-    starRating.jRating({
-      type: 'big', // type of the rate.. can be set to 'small' or 'big'
-      length: 10, // nb of stars
-      rateMax: 10,
-      bigStarsPath: starUrl + 'stars.png',
-      smallStarsPath: starUrl + 'small.png',
-      sendRequest: false,
-      canRateAgain: true,
-      nbRates: 9999999,
-      onClick: function(element, rate) {
-        setUrlRank(rate);
-        $scope.$apply(function() {
-          $scope.ranking = rate;
-        });
-      }
-    });
-
-  }
+  // function createStarRating(starUrl) {
+  //   var starRating = $("#star_rating");
+  //   starRating.jRating({
+  //     type: 'big', // type of the rate.. can be set to 'small' or 'big'
+  //     length: 10, // nb of stars
+  //     rateMax: 10,
+  //     bigStarsPath: starUrl + 'stars.png',
+  //     smallStarsPath: starUrl + 'small.png',
+  //     sendRequest: false,
+  //     canRateAgain: true,
+  //     nbRates: 9999999,
+  //     onClick: function(element, rate) {
+  //       setUrlRank(rate);
+  //       $scope.$apply(function() {
+  //         $scope.ranking = rate;
+  //       });
+  //     }
+  //   });
+  //
+  // }
 
   function setUrlRank(rank) {
     var rank_data = {
@@ -433,15 +229,15 @@ panelApp.controller("PanelCtrl", function($scope, $document) {
 panelApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-    when('/features/all', {
-      templateUrl: 'partials/extracted-entities-partial.html'
-    }).
-    when('/features/domain', {
-      templateUrl: 'partials/domain-extracted-partial.html'
-    }).
-    when('/features/manual', {
-      templateUrl: 'partials/manual-features-partial.html'
-    }).
+    // when('/features/all', {
+    //   templateUrl: 'partials/extracted-entities-partial.html'
+    // }).
+    // when('/features/domain', {
+    //   templateUrl: 'partials/domain-extracted-partial.html'
+    // }).
+    // when('/features/manual', {
+    //   templateUrl: 'partials/manual-features-partial.html'
+    // }).
     when('/trail/explored', {
       templateUrl: 'partials/trail-based-explored-partial.html'
     }).
